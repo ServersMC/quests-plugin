94f0d1b1aab8886a311a942599d91a9739279614
Dennis Lysenko <dennislysenko@hotmail.com>
2016-10-06 22:43:25 -0400

    Cleared Test Quest

--- src/org/serversmc/quests/quests/TestQuest.java
+++ src/org/serversmc/quests/quests/TestQuest.java
@@ -3,9 +3,7 @@ package org.serversmc.quests.quests;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.serversmc.quests.types.Quest;
 
 public class TestQuest extends Quest implements Listener {
@@ -13,13 +11,9 @@ public class TestQuest extends Quest implements Listener {
 	public TestQuest() {
 		location = new Location(Bukkit.getWorld("world"), 156, 64, -455);
 	}
-
+	
 	@Override
 	public void init(Player player) {
-	}
-
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
 	}
 
 }
