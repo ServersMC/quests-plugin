b5965ee4f1148cab6cf89d0e1a361619d83ae9be
Dennis Lysenko <dennislysenko@hotmail.com>
2016-10-06 22:51:55 -0400

    Fixed error on clicking air

--- src/org/serversmc/quests/events/QuestEvent.java
+++ src/org/serversmc/quests/events/QuestEvent.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.serversmc.quests.types.Quest;
 
@@ -12,6 +13,9 @@ public class QuestEvent implements Listener {
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
+		if (!event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+			return;
+		}
 		Location location = event.getClickedBlock().getLocation();
 		
 		for (Quest quest : Quest.quests) {
